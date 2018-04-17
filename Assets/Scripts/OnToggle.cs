f07543bea72b97b5eb90f0ebb2b493a59e29bfce
Jukka Hilvonen <jukkahilvonen@gmail.com>
2018-04-17 21:54:10 +0300

    Added button toggle and physic control toggles to control script and OnToggle.cs

---
+++ Assets/Scripts/OnToggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnToggle : MonoBehaviour
+{
+
+    //Attach this script to a Toggle GameObject. To do this, go to Create>UI>Toggle.
+    //Set your own Text in the Inspector window
+    Toggle m_Toggle;
+    public Text m_Text;
+
+    void Start()
+    {
+        //Fetch the Toggle GameObject
+        m_Toggle = GetComponent<Toggle>();
+        //Add listener for when the state of the Toggle changes, to take action
+        m_Toggle.onValueChanged.AddListener(delegate
+        {
+            ToggleValueChanged(m_Toggle);
+        });
+
+        //Initialise the Text to say the first state of the Toggle
+        m_Text.text = "First Value : " + m_Toggle.isOn;
+    }
+
+    //Output the new state of the Toggle into Text
+    void ToggleValueChanged(Toggle change)
+    {
+        m_Text.text = "Controls : " + m_Toggle.isOn;
+    }
+}
